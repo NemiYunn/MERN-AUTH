a1f878eaf8931fbbedd3955b71975bbbd5be3991
Nethmi Marasinghe <115603849+NemiYunn@users.noreply.github.com>
2024-01-02 10:07:51 +0530

    Resolve some problems

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter , Routes , Route} from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Signin from "./pages/Signin"
-import SignUp from "./pages/signUp"
+import Signin from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,18 +1,27 @@
+import React from 'react'
 import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+
+  const [formData , setFormData] = React.useState({});
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.id]:e.target.value})
+  }
+  // console.log(formData);
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
         Sign Up
       </h1>
-      <form className='flex flex-col gap-4'>
+      <form  className='flex flex-col gap-4'>
         <input type="text" name="" id="username" placeholder='Username'
-        className='bg-slate-100 p-3 rounded-lg' />
+        className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
         <input type="email" name="" id="email" placeholder='Email'
-        className='bg-slate-100 p-3 rounded-lg' />
+        className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
         <input type="password" name="" id="password" placeholder='Password'
-        className='bg-slate-100 p-3 rounded-lg' />
+        className='bg-slate-100 p-3 rounded-lg' onChange={handleChange}/>
 
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           Sign Up
@@ -26,4 +35,4 @@ export default function SignUp() {
       </div>
     </div>
   )
-}
+  }
