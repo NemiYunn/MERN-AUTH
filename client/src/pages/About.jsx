8a75d5da49385a967112c129b4126635972b5869
Nethmi Marasinghe <115603849+NemiYunn@users.noreply.github.com>
2024-01-12 11:37:43 +0530

    Complete home and about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+      About
+    </h1>
+    <p className='mb-4 text-slate-700'>
+      Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Fusce semper nibh nunc,
+      quis condimentum tellus vehicula sed.
+      Nam in volutpat est. Donec tempor pulvinar
+      venenatis. 
+    </p>
+    <p className='mb-4 text-slate-700'>
+      Nunc et tortor vitae nisi accumsan ornare. Vivamus orci eros,
+      pretium et ex vitae, tempus dapibus sem. Mauris sed efficitur libero.
+      Nunc at turpis nec lectus convallis dignissim eu a neque.
+      Praesent sit amet vestibulum nulla.
+      
+    </p>
+    <p className='mb-4 text-slate-700'>
+      Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam euismod, nisl sed hendrerit venenatis, diam tortor molestie velit,
+      ut tristique nunc purus et leo. 
+    </p>
+  </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        Welcome to my Auth App!
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Fusce semper nibh nunc,
+        quis condimentum tellus vehicula sed.
+        Nam in volutpat est. Donec tempor pulvinar
+        venenatis. Phasellus nec nunc sollicitudin,
+        bibendum nulla quis, laoreet felis.
+        Nunc luctus mauris vitae augue laoreet
+        imperdiet. Maecenas sagittis finibus
+        nulla sed venenatis. Sed vulputate,
+        tellus ut condimentum tempor,
+        sem massa auctor erat, eleifend
+        molestie diam sem in est.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Nunc et tortor vitae nisi accumsan ornare. Vivamus orci eros,
+        pretium et ex vitae, tempus dapibus sem. Mauris sed efficitur libero.
+        Nunc at turpis nec lectus convallis dignissim eu a neque.
+        Praesent sit amet vestibulum nulla.
+        In mi turpis, tincidunt quis egestas vel, pretium nec dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam euismod, nisl sed hendrerit venenatis, diam tortor molestie velit,
+        ut tristique nunc purus et leo. Praesent tempor libero vel tortor cursus auctor. Aliquam elementum vitae nunc ut iaculis. Duis pretium nunc et arcu gravida consectetur. Fusce sed lorem neque. Curabitur est magna, tristique a ultricies ut, euismod et mi.
+      </p>
+    </div>
   )
 }
