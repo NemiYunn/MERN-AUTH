bdb1a01c2c260016235396e7f996d638b61f889d
Nethmi Marasinghe <115603849+NemiYunn@users.noreply.github.com>
2024-01-12 07:30:12 +0530

    Completed user update functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -60,7 +60,8 @@ export default function SignIn() {
         <input type="password" name="" id="password" placeholder='Password'
           className='bg-slate-100 p-3 rounded-lg' onChange={handleChange} />
 
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        <button 
+        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? "Loading.." : "Sign In"}
         </button>
         <OAuth />
